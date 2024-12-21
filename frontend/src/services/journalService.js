251249e858321326e64671bbffbd65278f89746e
Nikita Verma <161702924+nikitaverma18@users.noreply.github.com>
2024-12-21 18:42:48 +0530

    Update journalService.js

--- frontend/src/services/journalService.js
+++ frontend/src/services/journalService.js
@@ -1,7 +1,7 @@
 // src/services/journalService.js
 import axios from 'axios';
 
-const API_URL = 'https://ai-powered-journal-app-4.onrender.com'; 
+const API_URL = 'https://ai-powered-journal-app-5.onrender.com'; 
 
 export const getJournals = async () => {
   try {

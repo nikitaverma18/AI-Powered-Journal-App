de7083cfa451557c8a5f5cdb605b67fcdf0a3b97
Nikita Verma <161702924+nikitaverma18@users.noreply.github.com>
2025-02-03 16:57:12 +0530

    Update JournalEntry.js

--- frontend/src/components/JournalEntry.js
+++ frontend/src/components/JournalEntry.js
@@ -11,8 +11,26 @@ const JournalEntry = ({ entry, onDelete, onEdit }) => {
   };
 
   const handleSaveEdit = () => {
+    if(!newContent.trim()){
+      alert('Entry Cannot Be Empty!');
+      return;
+    }
     onEdit(entry._id, newContent);
     setIsEditing(false);
+    alert('Edit Saved Successfully!');
+  };
+
+  const handleCancelEdit = () =>{
+    setNewContent(entry.content);
+    setIsEditing(false);
+  }
+
+  const handleDelete = () => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this entry?');
+    if(confirmDelete){
+      onDelete(entry._id);
+      alert('Enrty delete successfully!');
+    }
   };
 
   return (
@@ -21,6 +39,7 @@ const JournalEntry = ({ entry, onDelete, onEdit }) => {
         <td colSpan="4">
           <textarea value={newContent} onChange={handleEditChange} placeholder="Edit your entry..." />
           <button onClick={handleSaveEdit}>Save</button>
+          <button onClick={handleCancelEdit} className='cancel-button'>Cancel</button>
         </td>
       ) : (
         <>
@@ -31,7 +50,7 @@ const JournalEntry = ({ entry, onDelete, onEdit }) => {
             <button onClick={() => setIsEditing(true)} className="icon-button">
               <FaEdit />
             </button>
-            <button onClick={() => onDelete(entry._id)} className="icon-button">
+            <button onClick={handleDelete} className="icon-button">
               <FaTrash />
             </button>
           </td>
